f874d5f624c611d58b5a818db9fa237f5320d359
spectator <39882187+SSHpectator@users.noreply.github.com>
2026-01-12 22:17:57 +0100

    Fix formatting and comments in portCheck.py

--- checks/portCheck.py
+++ checks/portCheck.py
@@ -3,7 +3,7 @@ import socket
 FTP_PORT = 21
 
 """
-checkPort try to connect to the ftp0s port (21),
+checkPort try to connect to the ftp's port (21),
 if it can estabilish a connection then it 
 returns true,
 otherwise false
@@ -14,4 +14,5 @@ def checkPort(host):
         s.connect((host, FTP_PORT))
         return True
     except:
+
         return False
